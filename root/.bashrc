be09419579b43b96a0b0f403bd8a749bfbdbff9e
Ricardo González Moreno <correoricky@gmail.com>
2016-09-30 09:45:31 +0200

    Modify to user User CCache and Portage CCache

---
+++ root/.bashrc
@@ -0,0 +1,1 @@
+unset CCACHE_DIR
--- usr/local/bin/tmpfs-ccache-user.sh
+++ usr/local/bin/tmpfs-ccache-user.sh
@@ -3,9 +3,6 @@
 # Load config variables
 . /etc/tmpfs-ccache
 
-echo export CCACHE_DIR=$TMPFS
-export CCACHE_DIR=$TMPFS
+export CCACHE_DIR=$USERTMPFS
 
-echo ccache -F 0 -M "$SIZE"G
-ccache -F 0 -M "$SIZE"G
-
+ccache -F 0 -M $USERSIZE &> /dev/null
